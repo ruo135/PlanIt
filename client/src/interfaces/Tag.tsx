893f2d9e71c9c9dba4c8fdcf61409f88097e78d7
lucaschew <lucaschew0@gmail.com>
2024-12-09 16:05:12 -0500

    Added models for data types

---
+++ client/src/interfaces/Tag.tsx
@@ -0,0 +1,7 @@
+export type Tag = {
+  id?: string
+  userId: string
+  name: string
+  color: string
+  isVisible: boolean
+}
---
+++ client/src/interfaces/Theme.tsx
@@ -0,0 +1,5 @@
+export type Theme = {
+  id?: string
+  userId: string
+  theme: string
+}
